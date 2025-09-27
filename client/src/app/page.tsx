769f371a2e3196b116c89765434e6c421d8d05ad
Ibrahim Chaudhary <ibrahimchaudhary421@gmail.com>
2025-09-28 03:51:33 +0500

    auth done

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
 
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by GHULUL{" "}
+            Get started by GHULAM{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               src/app/page.tsx
             </code>
